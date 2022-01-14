20f23d855b072337fb8abcabe3badeb21a6195c3
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-13 22:10:02 -0600

    added api testing

--- api.test.js
+++ api.test.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const { test, expect, describe } = require('@jest/globals');
 const axios = require('axios');
 
+const parkDataKeys = ['totalResults', 'totalPages', 'dataStart', 'dataEnd', 'currentPage', 'results'];
+const parkDataResultKeys = ['fullname', 'parkcode', 'states', 'designation'];
+
 describe('Outdoor Adventures Integration and Peformance Testing', () => {
   describe('Making sure api is up', () => {
     test('api status', () => {
@@ -18,5 +21,30 @@ describe('Outdoor Adventures Integration and Peformance Testing', () => {
     });
   });
 
-  describe('');
+  describe('Route tests', () => {
+    jest.setTimeout(30000);
+    test('Park Data', () => {
+      return axios
+        .get(`${process.env.API_URL}/api/parks?page=1&limit=10`)
+        .then(({ status, data }) => {
+          expect(status).toBe(200);
+
+          parkDataKeys.forEach((key) => {
+            expect(data).toHaveProperty(key);
+          });
+
+          data.results.forEach((parkData) => {
+            parkDataResultKeys.forEach((key) => {
+              expect(parkData).toHaveProperty(key);
+              expect(typeof parkData[key]).toBe('string');
+              expect(parkData[key].length).toBeGreaterThan(0);
+            });
+          });
+        })
+        .catch((err) => {
+          console.log('this ran');
+          console.log(err);
+        });
+    });
+  });
 });
