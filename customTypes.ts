fa165f1c8c4548e48e5dd974e5f0814c0af45427
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-15 08:42:22 -0600

    reformatted test file to run with typescript

---
+++ customTypes.ts
@@ -0,0 +1,25 @@
+export type parkTableData = {
+  fullname: string;
+  parkcode: string;
+  states: string;
+  designation: string;
+};
+
+export type parksPaginatedResult = {
+  totalResults: number;
+  totalPages: number;
+  dataStart: number;
+  dataEnd: number;
+  currentPage: number;
+  results: parkTableData[];
+};
+
+export type parksApiRouteResponse = {
+  status: number;
+  data: parksPaginatedResult;
+};
+
+export type apiTestResponse = {
+  status: number;
+  data: { message: string };
+};
---
+++ utils.ts
@@ -0,0 +1,34 @@
+export const calculateDataEnd = (page: number, limit: number, totalResults: number) => {
+  if (page * limit > totalResults) {
+    return totalResults;
+  }
+
+  return page * limit;
+};
+
+export const calculateNumResult = (page: number, limit: number, totalResults: number) => {
+  if (page * limit > totalResults) {
+    return totalResults - (page - 1) * limit;
+  }
+
+  return limit;
+};
+
+export const parkDataParameters = [
+  { page: 1, limit: 10 },
+  { page: 1, limit: 25 },
+  { page: 1, limit: 50 },
+  { page: 2, limit: 10 },
+  { page: 2, limit: 25 },
+  { page: 2, limit: 50 },
+  { page: 3, limit: 50 },
+  { page: 4, limit: 50 },
+  { page: 5, limit: 50 },
+  { page: 6, limit: 50 },
+  { page: 7, limit: 50 },
+  { page: 8, limit: 50 },
+  { page: 9, limit: 50 },
+  { page: 10, limit: 50 },
+];
+
+export const parkDataResultKeys = ['fullname', 'parkcode', 'states', 'designation'];
