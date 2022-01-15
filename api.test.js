d6e3fc464c769ed30b56578259807c06a392993b
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-15 07:51:11 -0600

    created way to test parks route

--- api.test.js
+++ api.test.js
@@ -5,41 +5,33 @@ const axios = require('axios');
 
 const parkDataKeys = ['totalResults', 'totalPages', 'dataStart', 'dataEnd', 'currentPage', 'results'];
 const parkDataResultKeys = ['fullname', 'parkcode', 'states', 'designation'];
+const parkDataParameters = [
+  { page: 1, limit: 10 },
+  { page: 1, limit: 25 },
+  { page: 1, limit: 50 },
+];
 
 describe('Outdoor Adventures Integration and Peformance Testing', () => {
   describe('Making sure api is up', () => {
     test('api status', () => {
-      return axios
-        .get(`${process.env.API_URL}/api/testconnection`)
-        .then(({ status, data }) => {
-          expect(status).toBe(200);
-          expect(data.message).toBe('api up and running');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      return axios.get(`${process.env.API_URL}/api/testconnection`).then(({ status, data }) => {
+        expect(status).toBe(200);
+        expect(data.message).toBe('api up and running');
+      });
     });
   });
 
-  describe('Route tests', () => {
+  describe('Parks route tests', () => {
     jest.setTimeout(30000);
-    test('Park Data', () => {
-      return axios.get(`${process.env.API_URL}/api/parks?page=1&limit=10`).then(({ status, data }) => {
+
+    test.each(parkDataParameters)(`Parks route called with page: $page and limit: $limit`, ({ page, limit }) => {
+      return axios.get(`${process.env.API_URL}/api/parks?page=${page}&limit=${limit}`).then(({ status, data }) => {
         expect(status).toBe(200);
-
-        parkDataKeys.forEach((key) => {
-          expect(data).toHaveProperty(key);
-        });
-
-        data.results.forEach((parkData) => {
-          parkDataResultKeys.forEach((key) => {
-            expect(parkData).toHaveProperty(key);
-            expect(typeof parkData[key]).toBe('string');
-            expect(parkData[key].length).toBeGreaterThan(0);
-          });
-        });
+        expect(data.totalPages).toBe(Math.ceil(data.totalResults / limit));
+        expect(data.totalResults).toBe(465);
+        expect(data.dataStart).toBe(1);
+        expect(data.results.length).toBe(limit);
       });
-      // Can't put .catch because it will catch the jest errors, causing the tests to not fail
     });
   });
 });
